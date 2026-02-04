297a5a1389b2e5c7924f97be4d549e2369ec9d62
v0ncent <83373495+v0ncent@users.noreply.github.com>
2026-02-03 19:45:31 -0600

    added .env and config file

---
+++ EpsteinFilesParser/src/main/java/Config.java
@@ -0,0 +1,9 @@
+import io.github.cdimascio.dotenv.Dotenv;
+
+public final class Config {
+    private static final Dotenv dotenv = Dotenv.load(); //dotenv loads our config
+
+    public static String get(String key){
+        return dotenv.get(key);
+    }
+}
